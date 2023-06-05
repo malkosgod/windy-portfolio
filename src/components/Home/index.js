a8400615128c5dc34a12da85c402864fcb160bf3
Windy Windy <70558316+malkosgod@users.noreply.github.com>
2023-06-05 13:50:18 +1000

    Logo Update
    Logo Update 1

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-s.png'
-import Logo from './Logo'
+/* import Logo from './Logo' */
 import './index.scss'
 
 const Home = () => {
@@ -63,7 +63,7 @@ const Home = () => {
                         CONTACT ME
                     </Link>
                 </div>
-                <Logo />
+                {/* <Logo /> */}
             </div>
         </>
     )
